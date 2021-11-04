0e56aaa07b0c940262861111289d975bb3625310
Myungho96 <59616266+Myungho96@users.noreply.github.com>
2021-11-04 21:51:00 +0900

    Update AddressController.java

--- carrot_market/src/main/java/com/example/demo/src/address/AddressController.java
+++ carrot_market/src/main/java/com/example/demo/src/address/AddressController.java
@@ -35,25 +35,15 @@ public class AddressController {
 
     /**
      * 검색을 통해 동네 조회
-<<<<<<< HEAD
-     * [GET] /address?search={search}&townId={townId}
-=======
      * [GET] /address?search={search}
->>>>>>> hoy_branch
      * @return BaseResponse<List<GetTownRes>>
      * 토큰 필요함 -> 토큰의 유저정보를 통해 승인된 주소 정보가 있는지 확인해야함
      */
     @ResponseBody
     @GetMapping("")
-<<<<<<< HEAD
-    public BaseResponse<List<GetTownRes>> getTownSearchBySearch(@RequestParam("search") String search, @RequestParam("townId") int townId ) {
-        try{
-            List<GetTownRes> getTownRes = addressProvider.getTownBySearch(search, townId);
-=======
     public BaseResponse<List<GetTownRes>> getTownSearchBySearch(@RequestParam("search") String search) {
         try{
             List<GetTownRes> getTownRes = addressProvider.getTownBySearch(search);
->>>>>>> hoy_branch
             return new BaseResponse<>(getTownRes);
         } catch(BaseException exception){
             return new BaseResponse<>((exception.getStatus()));
@@ -62,26 +52,15 @@ public class AddressController {
     }
 
     /**
-<<<<<<< HEAD
-     * 현재 위치를 통해 주변 동네 조회
-     * [GET] /address/location?townId={townId}
-=======
      * 현재 위치를 통해 동네 조회
      * [GET] /address/location
->>>>>>> hoy_branch
      * @return BaseResponse<List<GetTownRes>>
      */
     @ResponseBody
     @GetMapping("/location")
-<<<<<<< HEAD
-    public BaseResponse<List<GetTownRes>> getTownSearchByLocation(@RequestParam("townId") int townId) {
-        try{
-            List<GetTownRes> getTownRes = addressProvider.getTownByLocation(townId);
-=======
     public BaseResponse<List<GetTownRes>> getTownSearchByLocation(@RequestBody GetTownReq getTownReq) {
         try{
             List<GetTownRes> getTownRes = addressProvider.getTownByLocation(getTownReq);
->>>>>>> hoy_branch
             return new BaseResponse<>(getTownRes);
         } catch(BaseException exception){
             return new BaseResponse<>((exception.getStatus()));
@@ -89,43 +68,4 @@ public class AddressController {
 
     }
 
-<<<<<<< HEAD
-    /**
-     * 현재 위치한 동네에 해당하는 동네id 조회
-     * [GET] /address/townId?city={city}&district={district}&townName={townName}
-     * @return BaseResponse<List<GetTownRes>>
-     */
-    @ResponseBody
-    @GetMapping("/townId")
-    public BaseResponse<Integer> getTownIdByCurrentLocation(@RequestParam("city") String city, @RequestParam("district") String district, @RequestParam("townName") String townName) throws BaseException {
-
-        try{
-            int townId = addressProvider.getTownId(city, district, townName);
-            return new BaseResponse<>(townId);
-        } catch(BaseException exception){
-            return new BaseResponse<>((exception.getStatus()));
-        }
-    }
-
-
-    /**
-     * 특정 동네의 range 별 근처 동네 리스트 반환
-     * [GET] /address/near?townId={townId}
-     * @return BaseResponse<List<GetTownRes>>
-     */
-    @ResponseBody
-    @GetMapping("/near")
-    public BaseResponse<GetNearTownListRes> getNearTownLiST(@RequestParam("townId") int townId) throws BaseException {
-        try{
-            GetNearTownListRes getNearTownListRes  = addressProvider.getNearTownList(townId);
-            return new BaseResponse<>(getNearTownListRes);
-        } catch(BaseException exception){
-            return new BaseResponse<>((exception.getStatus()));
-        }
-    }
-
-
-
-=======
->>>>>>> hoy_branch
 }
