3a46c40c3df1f52eb4159fadd2e8b94c90e3871d
myungho96 <myungho96@github.com>
2021-11-05 15:40:40 +0900

    add [GET]wish-list, [POST]wish-list, [GET]wish-list/count/:postId

---
+++ carrot_market/src/main/java/com/example/demo/src/wishList/WishListController.java
@@ -0,0 +1,81 @@
+package com.example.demo.src.wishList;
+
+import com.example.demo.src.post.model.AllPostSelectRes;
+import com.example.demo.src.user.model.PostLoginReq;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.wishList.model.*;
+import com.example.demo.utils.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wish-list")
+public class WishListController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final WishListProvider wishListProvider;
+    @Autowired
+    private final WishListService wishListService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+
+
+    public WishListController(WishListProvider wishListProvider, WishListService wishListService, JwtService jwtService){
+        this.wishListProvider = wishListProvider;
+        this.wishListService = wishListService;
+        this.jwtService = jwtService;
+    }
+
+    /**
+     * 관심목록 조회 API
+     * [GET] /wish-list
+     * @return BaseResponse<WishListSelectRes>
+     */
+    @ResponseBody
+    @GetMapping("")
+    public BaseResponse<List<WishListSelectRes>> wishListSelect(){
+        try{
+            List<WishListSelectRes> wishListSelectRes = wishListProvider.wishListSelect();
+            return new BaseResponse<>(wishListSelectRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
+    }
+    //게시물의 관심 목록 개수
+    @ResponseBody
+    @GetMapping("/count/{postId}")
+    public BaseResponse<WishListCount> wishListCount(@PathVariable("postId") int postId){
+        try{
+            WishListCount wishListCount = wishListProvider.wishListCount(postId);
+            return new BaseResponse(wishListCount);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
+    }
+
+    //관심목록 추가
+    @ResponseBody
+    @PostMapping("")
+    public BaseResponse<PostWishListRes> postWishListRes(@RequestBody PostWishListReq postWishListReq){
+        try{
+            PostWishListRes postWishListRes = wishListService.createWishList(postWishListReq);
+            return new BaseResponse(postWishListRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+
+    }
+
+
+}
---
+++ carrot_market/src/main/java/com/example/demo/src/wishList/model/WishListCount.java
@@ -0,0 +1,13 @@
+package com.example.demo.src.wishList.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class WishListCount {
+    private int postId;
+    private int wishListCount;
+}
