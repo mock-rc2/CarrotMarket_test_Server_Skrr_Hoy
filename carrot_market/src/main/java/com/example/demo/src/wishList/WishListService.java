c51da2277366618c27ef8b8d887871b942915e7b
myungho96 <myungho96@github.com>
2021-11-05 18:07:58 +0900

    add remove wishList

--- carrot_market/src/main/java/com/example/demo/src/wishList/WishListService.java
+++ carrot_market/src/main/java/com/example/demo/src/wishList/WishListService.java
@@ -2,7 +2,6 @@ package com.example.demo.src.wishList;
 
 
 import com.example.demo.config.BaseException;
-import com.example.demo.src.user.model.PostUserRes;
 import com.example.demo.src.wishList.model.*;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
@@ -56,6 +55,31 @@ public class WishListService {
         }
 
     }
+    public void modifyWishListStatus(PatchWishListStatus patchWishListStatus) throws BaseException {
+        try{
+            int checkUserId = wishListDao.checkUserId(patchWishListStatus.getUserId());
+            if(checkUserId == 0){//정상 상태가 아닌 유저라면
+                throw new BaseException(POST_POST_INVALID_USER);
+            }
+            int checkStatus = wishListDao.checkStatus(patchWishListStatus.getWishListId());
+            if(checkStatus == 0){//이미 삭제됐는지
+                throw new BaseException(MODIFY_FAIL_INVALID_STATUS);
+            }
+
+            int checkUserWish = wishListDao.checkUserWish(patchWishListStatus);
+            if(checkUserWish == 0){//이미 삭제됐는지
+                throw new BaseException(MODIFY_FAIL_INVALID_USER_WISHLIST);
+            }
+
+            int result = wishListDao.modifyWishListStatus(patchWishListStatus);
+            if(result == 0){
+                throw new BaseException(MODIFY_FAIL_WISHLIST_STATUS);
+            }
+        } catch(Exception exception){
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
 
 
 
