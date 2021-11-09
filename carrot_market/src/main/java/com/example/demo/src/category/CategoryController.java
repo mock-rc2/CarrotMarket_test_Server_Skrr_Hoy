1f6cc5d89f92427d69154c2e94ea7f1b01f1ed30
seongwoo <seongwoo0513@uos.ac.kr>
2021-11-10 02:41:45 +0900

    Add category template

---
+++ carrot_market/src/main/java/com/example/demo/src/category/CategoryController.java
@@ -0,0 +1,33 @@
+package com.example.demo.src.category;
+
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final CategoryProvider categoryProvider;
+    @Autowired
+    private final CategoryService categoryService;
+    @Autowired
+    private final JwtService jwtService;
+
+
+
+
+    public CategoryController(CategoryProvider categoryProvider, CategoryService categoryService, JwtService jwtService){
+        this.categoryProvider = categoryProvider;
+        this.categoryService = categoryService;
+        this.jwtService = jwtService;
+    }
+
+
+
+
+}
---
+++ carrot_market/src/main/java/com/example/demo/src/category/CategoryDao.java
@@ -0,0 +1,23 @@
+package com.example.demo.src.category;
+
+
+import com.example.demo.src.category.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class CategoryDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+
+}
